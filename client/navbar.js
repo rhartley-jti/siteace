85b1d3936c219fda1d16eb7d85d17a9bb709c5bc
Ryan Hartley <ryan.hartley@gmail.com>
2016-02-06 10:20:59 -0700

    Can now clear the search

--- client/navbar.js
+++ client/navbar.js
@@ -1,3 +1,9 @@
+Template.navbar.helpers({
+	canAddSite: function() {
+		return Meteor.userId();
+	}
+});
+
 Template.navbar.events({
 	"click .js-add-website": function(event) {
 		$("#url").val($("#navUrl").val());
--- client/search_form.js
+++ client/search_form.js
@@ -1,6 +1,18 @@
+Template.search_form.helpers({
+	hasSearchText: function() {
+		return Session.get("hasSearchText");
+	}
+});
+
 Template.search_form.events({
 	"keyup #search-box": _.throttle(function(event) {
 		var text = event.target.value.trim();
+		Session.set("hasSearchText", text.length > 0);
 		WebsiteSearch.search(text);
-	}, 200)
+	}, 200),
+	"click .js-search-clear": function(event) {
+		$("#search-box").val("");
+		Session.set("hasSearchText", false);
+		WebsiteSearch.search("");
+	}
 });
